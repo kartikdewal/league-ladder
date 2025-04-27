a0e82b1fa304f80061d817ba2fc8558307cccdc3
Kartik Dewal <kartikeya.dewal@gmail.com>
2025-04-28 00:56:02 +0530

    fix: missing package name

--- src/main/java/dev/kartikdewal/leagueladder/config/UserDetailsConfig.java
+++ src/main/java/dev/kartikdewal/leagueladder/config/UserDetailsConfig.java
@@ -1,3 +1,5 @@
+package dev.kartikdewal.leagueladder.config;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
