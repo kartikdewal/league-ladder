afe43ef9db3dadea4e9c76a4f8a31186f2c13746
Kartik Dewal <kartikeya.dewal@gmail.com>
2025-04-27 23:38:13 +0530

    feat: add cors config

---
+++ src/main/java/dev/kartikdewal/leagueladder/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package dev.kartikdewal.leagueladder.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedHeader("Content-Type");
+        config.addAllowedHeader("Authorization");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // Apply CORS settings to all endpoints
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
