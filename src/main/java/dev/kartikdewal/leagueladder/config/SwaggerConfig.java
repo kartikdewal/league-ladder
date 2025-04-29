61544dfa08eefd89fe9553858332da69b7b63dde
Kartik Dewal <kartikeya.dewal@gmail.com>
2025-04-29 22:14:38 +0530

    feat: add swagger

---
+++ src/main/java/dev/kartikdewal/leagueladder/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package dev.kartikdewal.leagueladder.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("League Ladder Service API")
+                        .version("1.0")
+                        .description("API documentation for the League Ladder Service"));
+    }
+}
